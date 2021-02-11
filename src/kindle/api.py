3d8c8ef4528c5d1808ec03fbf76d1812dfea28e2
mkb79 <mkb79@hackitall.de>
2021-02-11 11:28:51 +0100

    ADD: api endpoints founds

--- src/kindle/api.py
+++ src/kindle/api.py
@@ -1,3 +1,7 @@
+# found api endpoints
+# have to find out more
+
+
 import json
 from datetime import datetime
 
@@ -41,3 +45,73 @@ def get_manifest(auth, asin: str):
         return r.json()
 
 
+def whispersync(auth):
+    user_id = auth.customer_info["user_id"]
+    url = f"https://api.amazon.com/whispersync/v2/data/{user_id}/datasets"
+    params1 = {
+        "embed": "records.first_page",
+        "quiet": "true"
+    }
+    # alternative params in other requests found
+    params2 = {
+        "filterDeletedUpto": 201,
+        "embed": "records.first_page",
+        "filterDeleted": "true",
+        "after": 188
+    }
+    params3 = {
+        "after": 201,
+        "quiet": "true"
+    }
+    with httpx.Client(auth=auth) as session:
+        r = session.get(url, params=params1)
+        return r.json()
+
+
+def whispersync_records_by_identifier(auth, identifier: str):
+    user_id = auth.customer_info["user_id"]
+    url = f"https://api.amazon.com/whispersync/v2/data/{user_id}/datasets/{identifier}/records"
+    params = {
+        "after": 201
+    }
+    with httpx.Client(auth=auth) as session:
+        r = session.get(url, params=params)
+        return r.json()
+
+
+def sidecar(auth, asin: str):
+    url = f"https://sars.amazon.com/sidecar/sa/EBOK/{asin}"
+    with httpx.Client(auth=auth) as session:
+        r = session.get(url)
+        return r.json()
+
+
+def get_news(auth):
+    url = "https://sars.amazon.com/kinapps/notifications/new"
+    with httpx.Client(auth=auth) as session:
+        r = session.get(url)
+        return r.json()
+
+
+def get_notification_channels(auth, marketplace: str):
+    # marketplace e.g. A1PA6795UKMFR9
+    url = f"https://d3ohh9b4v3oawh.cloudfront.net/iOS/1.1/{marketplace}/notificationsChannels.json"
+    with httpx.Client(auth=auth) as session:
+        r = session.get(url)
+        return r.json()
+
+
+def get_device_credentials(auth):
+    # gives same credential types like a device registration
+    # value are different, but why?
+    # credentials from device registration are still valid
+    # gives cookies for more amazon domains
+    url = "https://firs-ta-g7g.amazon.com/FirsProxy/getDeviceCredentials"
+    params = {
+        "softwareVersion": "1184366692"
+    }
+    with httpx.Client(auth=auth) as session:
+        r = session.get(url, params=params)
+        cred = xmltodict.parse(r.text)
+        cred = json.loads(json.dumps(cred))
+        return cred.get("response", cred)
